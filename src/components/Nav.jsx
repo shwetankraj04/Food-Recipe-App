d7c1618cc75e0e15ed149bdd7e1181ba66e7afad
Shwetank Raj <98588227+shwetankraj04@users.noreply.github.com>
2026-01-05 17:04:45 +0530

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
 import styles from "./nav.module.css";
 export default function Nav() {
-  return <div className={styles.nav}> 🍔 Receipe App</div>;
+  return <div className={styles.nav}> 🍔 Recipe App</div>;
 }
